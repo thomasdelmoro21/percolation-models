6e7d7abdf952e0894dc456313c5c5d14c66485e4
thomas <thomas.delmoro@stud.unifi.it>
2024-04-24 21:24:47 +0200

    Completed ForestFire

--- ForestFire.py
+++ ForestFire.py
@@ -9,19 +9,25 @@ Burned Tree: 2
 """
 
 import random
+import statistics
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 width = 200
 height = 200
 eight_neighbors = False
-density = 0.6
 init_burned = 1
-num_simulations = 5
-seed = None
+
+# only if you run one simulation
+density_of_trees = 0.6
+
+# only if you run multiple simulations
+num_densities = 20  # number of different density value to test
+num_runs = 5  # number of runs per density value
 
 
-def setup(num_burned=1):
+def setup(density, num_burned=1):
     grid = np.zeros((height, width))
     num_trees = int(density * width * height)
     burned_indices = np.random.choice(range(width * height), size=num_trees, replace=False)
@@ -65,21 +71,22 @@ def step(grid, burned_trees):
     return new_burned
 
 
-def simulate():
+def simulate(density, num_burned):
     active = True
-    grid, burned_trees = setup(init_burned)
+    grid, burned_trees = setup(density, num_burned)
     burned_data = []
     with plt.ion():
         fig = plt.figure()
+        plt.title("Density = {}".format(round(density, 3)))
         im = plt.imshow(grid, cmap="summer", vmin=0, vmax=2)
         while active:
             new_burned = step(grid, burned_trees)
-            if len(new_burned) > 0:
+            if len(new_burned) == 0:
+                active = False
+            else:
                 for tree in new_burned:
                     burned_trees.append(tree)
-                burned_data.append(len(burned_trees))
-            else:
-                active = False
+            burned_data.append(len(burned_trees))
 
             im.set_data(grid)
             fig.canvas.flush_events()
@@ -92,13 +99,26 @@ def plot_single_simulation(burned_data):
     plt.plot(burned_data, '-o', lw=1)
     plt.xlabel("Time")
     plt.ylabel("Number of Burned Trees")
-    plt.title("Forest Fire Simulation")
+    plt.title("Forest Fire")
     plt.show()
 
 
 def simulate_multiple():
-    pass
+    densities = np.linspace(0.1, 0.99, num_densities)
+    burned_results = []
+    for density in densities:
+        burned_data = []
+        for i in range(num_runs):
+            burned_data.append(simulate(density, init_burned)[-1])
+            plt.close()
+        burned_results.append(statistics.mean(burned_data))
+
+    plt.figure()
+    plt.plot(densities, burned_results, '-o', lw=1)
+    plt.xlabel("Density of Trees")
+    plt.ylabel("Burned Trees")
+    plt.title("Forest Fire")
+    plt.show()
 
 
-burned_data = simulate()
-plot_results(burned_data)
+simulate_multiple()
